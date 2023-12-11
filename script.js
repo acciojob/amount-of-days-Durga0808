59048ded389fb6ddda3a4a974271ade9a5e7542d
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2023-12-12 00:00:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-function daysOfAYear(year){
-	let leapYear=((year%4===0&&year%100!==0)||(year%400===0));
-	if(leapYear===true){
-		return 366;
-	}
-	else{
-		return 365;
+function daysOfAYear(year) {
+	if((year%4===0 && year%100!==0) || (year%400===0)){
+		return 366
+	}else{
+		return 365
 	}
 	
 }
-let year=prompt("Enter the Year");
-alert(daysOfAYear(year));
+const result=daysOfAYear(2022);
+console.log(result);
