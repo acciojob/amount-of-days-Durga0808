3dbdf81e1d2b817fafce7d3a2fa8d653fdb86bef
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2023-12-12 20:47:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-function daysOfAYear(year) {
-	if((year%4===0 && year%100!==0) || (year%400===0)){
-		return 366
-	}else{
-		return 365
-	}
-	
+function daysOfAYear(yer){
+    if(yer%4==0){
+        if(yer%100==0){
+            if(yer%400==0){
+                return 366
+            }
+            else{
+                return 365
+            }
+        }
+        return 366
+    }
+    else{
+        return 365
+    }
 }
-const result=daysOfAYear(prompt());
-alert(result);
